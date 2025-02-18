b8fa3381f5ae6779d077d500c97185262786a360
Ramz001 <rkenjaev1@gmail.com>
2025-02-18 11:02:59 +0500

    facebook login complete

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 import YandexProvider from "next-auth/providers/yandex";
+import FacebookProvider from "next-auth/providers/facebook";
 
 const handler = NextAuth({
   providers: [
@@ -9,12 +10,21 @@ const handler = NextAuth({
       clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET ?? "",
     }),
     YandexProvider({
-      clientId: process.env.NEXT_PUBLIC_AUTH_YANDEX_ID ?? "",
-      clientSecret: process.env.NEXT_PUBLIC_AUTH_YANDEX_SECRET ?? "",
+      clientId: process.env.NEXT_PUBLIC_YANDEX_ID ?? "",
+      clientSecret: process.env.NEXT_PUBLIC_YANDEX_SECRET ?? "",
+      authorization: {
+        params: {
+          force_confirm: "yes",
+        },
+      },
+    }),
+    FacebookProvider({
+      clientId: process.env.NEXT_PUBLIC_FACEBOOK_ID ?? "",
+      clientSecret: process.env.NEXT_PUBLIC_FACEBOOK_SECRET ?? "",
     }),
   ],
 });
 
-console.log(handler)
+console.log(handler);
 
 export { handler as GET, handler as POST };
